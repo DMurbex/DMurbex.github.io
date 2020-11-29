46e4b732f4ea83db7d40e49c63029efbb98a78ea
diegomorra <59740358+diegomorra@users.noreply.github.com>
2020-11-29 21:21:09 +0100

    Loading original files

---
+++ works.js
@@ -0,0 +1,10 @@
+
+function work01() {
+	document.getElementById("01").style.display = "block";
+	document.getElementById("02").style.display = "none";	
+}
+
+function work02() {
+	document.getElementById("01").style.display = "none";
+	document.getElementById("02").style.display = "block";	
+}
