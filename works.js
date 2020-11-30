65009e4c7ab6c438e57a0b2dc1b4a0c28e95f60f
diegomorra <59740358+diegomorra@users.noreply.github.com>
2020-11-30 16:45:56 +0100

    all

--- works.js
+++ works.js
@@ -1,10 +1,50 @@
 
 function work01() {
 	document.getElementById("01").style.display = "block";
-	document.getElementById("02").style.display = "none";	
+	document.getElementById("02").style.display = "none";
+    document.getElementById("03").style.display = "none";
+    document.getElementById("04").style.display = "none";
+    document.getElementById("05").style.display = "none";
+    document.getElementById("06").style.display = "none";
+    document.getElementById("07").style.display = "none";
+    document.getElementById("08").style.display = "none";
+    document.getElementById("09").style.display = "none";
+    document.getElementById("10").style.display = "none";
+    document.getElementById("11").style.display = "none";
+    document.getElementById("12").style.display = "none";
+    document.getElementById("13").style.display = "none";
+
 }
 
 function work02() {
 	document.getElementById("01").style.display = "none";
-	document.getElementById("02").style.display = "block";	
+	document.getElementById("02").style.display = "block";
+    document.getElementById("03").style.display = "none";
+    document.getElementById("04").style.display = "none";
+    document.getElementById("05").style.display = "none";
+    document.getElementById("06").style.display = "none";
+    document.getElementById("07").style.display = "none";
+    document.getElementById("08").style.display = "none";
+    document.getElementById("09").style.display = "none";
+    document.getElementById("10").style.display = "none";
+    document.getElementById("11").style.display = "none";
+    document.getElementById("12").style.display = "none";
+    document.getElementById("13").style.display = "none";
 }
+
+function work03() {
+	document.getElementById("01").style.display = "none";
+	document.getElementById("02").style.display = "none";
+    document.getElementById("03").style.display = "block";
+    document.getElementById("04").style.display = "none";
+    document.getElementById("05").style.display = "none";
+    document.getElementById("06").style.display = "none";
+    document.getElementById("07").style.display = "none";
+    document.getElementById("08").style.display = "none";
+    document.getElementById("09").style.display = "none";
+    document.getElementById("10").style.display = "none";
+    document.getElementById("11").style.display = "none";
+    document.getElementById("12").style.display = "none";
+    document.getElementById("13").style.display = "none";
+}
+
