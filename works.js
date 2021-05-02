527608c038194e268b51c2cfdb827597d261a33a
Chezonasiamo <75222620+DMurbex@users.noreply.github.com>
2021-05-03 00:38:34 +0200

    New site

--- works.js
+++ works.js
@@ -15,6 +15,7 @@ function work01() {
     document.getElementById("13").style.display = "none";
     document.html.scrollTop(); // For Safari
     document.documentElement.scrollTop(); //for chrome
+    
 
 }
 
@@ -53,4 +54,89 @@ function work03() {
     document.html.scrollTop(); // For Safari
     document.documentElement.scrollTop(); //for chrome
 }
+
+function work04() {
+	document.getElementById("01").style.display = "none";
+	document.getElementById("02").style.display = "none";
+    document.getElementById("03").style.display = "none";
+    document.getElementById("04").style.display = "block";
+    document.getElementById("05").style.display = "none";
+    document.getElementById("06").style.display = "none";
+    document.getElementById("07").style.display = "none";
+    document.getElementById("08").style.display = "none";
+    document.getElementById("09").style.display = "none";
+    document.getElementById("10").style.display = "none";
+    document.getElementById("11").style.display = "none";
+    document.getElementById("12").style.display = "none";
+    document.getElementById("13").style.display = "none";
+    document.html.scrollTop(); // For Safari
+    document.documentElement.scrollTop(); //for chrome
+}
+
+function work05() {
+	document.getElementById("01").style.display = "none";
+	document.getElementById("02").style.display = "none";
+    document.getElementById("03").style.display = "none";
+    document.getElementById("04").style.display = "none";
+    document.getElementById("05").style.display = "block";
+    document.getElementById("06").style.display = "none";
+    document.getElementById("07").style.display = "none";
+    document.getElementById("08").style.display = "none";
+    document.getElementById("09").style.display = "none";
+    document.getElementById("10").style.display = "none";
+    document.getElementById("11").style.display = "none";
+    document.getElementById("12").style.display = "none";
+    document.getElementById("13").style.display = "none";
+    document.html.scrollTop(); // For Safari
+    document.documentElement.scrollTop(); //for chrome
+}
+
+function work06() {
+	document.getElementById("01").style.display = "none";
+	document.getElementById("02").style.display = "none";
+    document.getElementById("03").style.display = "none";
+    document.getElementById("04").style.display = "none";
+    document.getElementById("05").style.display = "none";
+    document.getElementById("06").style.display = "block";
+    document.getElementById("07").style.display = "none";
+    document.getElementById("08").style.display = "none";
+    document.getElementById("09").style.display = "none";
+    document.getElementById("10").style.display = "none";
+    document.getElementById("11").style.display = "none";
+    document.getElementById("12").style.display = "none";
+    document.getElementById("13").style.display = "none";
+    document.html.scrollTop(); // For Safari
+    document.documentElement.scrollTop(); //for chrome
+}
+
+function work07() {
+	document.getElementById("01").style.display = "none";
+	document.getElementById("02").style.display = "none";
+    document.getElementById("03").style.display = "none";
+    document.getElementById("04").style.display = "none";
+    document.getElementById("05").style.display = "none";
+    document.getElementById("06").style.display = "none";
+    document.getElementById("07").style.display = "block";
+    document.getElementById("08").style.display = "none";
+    document.getElementById("09").style.display = "none";
+    document.getElementById("10").style.display = "none";
+    document.getElementById("11").style.display = "none";
+    document.getElementById("12").style.display = "none";
+    document.getElementById("13").style.display = "none";
+    document.html.scrollTop(); // For Safari
+    document.documentElement.scrollTop(); //for chrome
+}
+
+function menu_open() {
+    document.getElementById("sidemenu").style.display = "block";
+    document.getElementById("burger").style.display = "none";
+    
+}
+
+function menu_close() {
+    document.getElementById("sidemenu").style.display = "none";
+    document.getElementById("burger").style.display = "block";
+    
+}
+
 
